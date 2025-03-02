00c79c65f9ef82a49cba7300c4fad7939f39d016
natjam13 <murrayn2@sky.com>
2025-03-02 13:52:25 +0000

    injected the forecast API

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ function refreshWeather(response) {
     let date = new Date(response.data.time * 1000);
     let iconElement = document.querySelector("#icon");
 
-    
+
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     descriptionElement.innerHTML = response.data.condition.description;
@@ -39,7 +39,7 @@ function formatDate(date) {
      if (minutes < 10) {
          minutes - `0$(minutes)`;
      }
-     
+
      return `${day} ${hours}:${minutes}`;
 }
 
@@ -54,24 +54,24 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-   
-    searchCity(searchInput.value);
 
+    searchCity(searchInput.value);
 }
 
 function getForecast(city) {
     let apiKey = "6699b7b6dd40692473of31ft3a14835d";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}$units=metric`;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(displayForecast);
+
 }
 
-function displayForecast(response)  {
+function displayForecast(response){
     let days = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri'];
-    let forecastHTML = "";
+    let forecastHtml = "";
 
     days.forEach(function (day) {
-        forecastHTML =
-            forecastHTML +
+        forecastHtml =
+            forecastHtml +
             `
     <div class="weather-forecast-day">
         <div class="weather-forecast-date">${day}</div>
@@ -87,13 +87,12 @@ function displayForecast(response)  {
     `;
     });
 
+
     let forecastElement = document.querySelector("#forecast");
-    forecastElement.innerHTML = forecastHTML;
+    forecastElement.innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("London");
-displayForecast();
-
